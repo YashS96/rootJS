9da2b0d03f1517211799d691419ef481767a8429
Yash Shrivastava <yshrivastava@deloitte.com>
2021-11-08 10:15:00 +0530

    Refactored Async file

---
+++ Async/Async3_Observables.js
@@ -0,0 +1,11 @@
+//import * from "rxjs";
+//Promises are resolved only once! Cant call it again.
+const PromiseFunc=()=>{
+
+    return Promise.resolve().then(()=>{
+        //code!
+
+    })
+}
+
+//const ob=new  Rx.
